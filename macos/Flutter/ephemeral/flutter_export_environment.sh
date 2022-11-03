e5680e5609297062a44eb65b1c6269dfe4c79471
Valentin.E <98782476+ValentinEremia@users.noreply.github.com>
2022-11-03 13:57:53 +0200

    Add files via upload

---
+++ macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=D:\programare\flutter devs\flutter"
+export "FLUTTER_APPLICATION_PATH=C:\Users\Valentin\Desktop\news_by_nny"
+export "COCOAPODS_PARALLEL_CODE_SIGN=true"
+export "FLUTTER_BUILD_DIR=build"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
+export "DART_OBFUSCATION=false"
+export "TRACK_WIDGET_CREATION=true"
+export "TREE_SHAKE_ICONS=false"
+export "PACKAGE_CONFIG=.dart_tool/package_config.json"
